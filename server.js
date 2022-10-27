b350f8bb87c5816c0b9cfd29cc20bbb0d6dd0423
Sophia Aramburu <sophiaconstance12@gmail.com>
2022-10-26 20:44:54 -0800

    added propmts to server.js

--- server.js
+++ server.js
@@ -7,3 +7,22 @@ db.connect(err => {
     console.log('Database connected.');
     userPrompt();
 })
+
+var userPrompt = function () {
+    inquirer.prompt([
+        {   
+            type: "list",
+            name: "selection",
+            message: "Please select one of the following",
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add Department',
+                'Add Role',
+                'Add Employee',
+                'Update Employee Role',
+                'LogOut'
+            ]
+        }
+    ])
